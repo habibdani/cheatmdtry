5a1bb73a2a5a41e62f5dd099877c2bd3cc4b2530
M Habib Jufah Alhamdani <habibdani86@gmail.com>
2025-05-16 11:08:25 +0700

    Delete package.sh

--- package.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-version=$(cat mod.hjson |grep -e "^version:" |awk '{print $2}')
-zip -r super-cheat-v$version.zip README.md preview.png icon.png LICENSE mod.hjson bundles/ content/ scripts/ sounds/ sprites/ schematics/
